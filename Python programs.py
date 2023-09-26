1307b873ac69abb152368606f75e0598071f6283
Avantik23 <55268423+Avantik23@users.noreply.github.com>
2023-09-26 17:24:11 +0530

    Update Python programs.py

--- Python programs.py
+++ Python programs.py
@@ -1,29 +1,25 @@
-# def octal(n):
-#     print(oct(n))
+def octal(n):
+    print(oct(n))
 
-# def reverse_int(n):
-#     r=0
-#     while(n!=0):
-#         r=r*10+n%10
-#         n//=10
-#     print(r)
+def reverse_int(n):
+    r=0
+    while(n!=0):
+        r=r*10+n%10
+        n//=10
+    print(r)
 
-# def fib(n):
-#     if (n == 0):
-#         return 0
-#     elif(n == 1):
-#         return 1
-#     else:
-#         return fib(n-1)+fib(n-2)
+def fib(n):
+    if (n == 0):
+        return 0
+    elif(n == 1):
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
 
 
-# octal(29)
-# reverse_int(302)
-# for i in range(3):
-#     print(fib(i),end=" ")
+octal(29) #DECIMAL TO OCTAL
+reverse_int(302)# REVERSE A NUMBER
+for i in range(3):    #FIBONACCI SERIES
+    print(fib(i),end=" ")
 
-# print("\nFourth fibonacci number:",fib(4))
-import re
-s="John000Wick"
-a=re.split('0',s)
-print(a)
+print("\nFourth fibonacci number:",fib(4)) #NTH FIBONACCI NUMBER
